150e26e49ebb7d27b2199d05dbd9d1bac355a5ca
Niels Koster <n.koster@portavita.eu>
2020-12-06 17:35:01 +0100

    Style stuff

--- list.js
+++ list.js
@@ -2,14 +2,15 @@ typeof elX !== 'undefined' && elX.remove()
 elX = document.createElement('div')
 buttonX = document.createElement('button')
 buttonX.textContent = 'Exit Image List'
+buttonX.cssText = 'padding:40px'
 imgs = [...new Set(Array.from(document.images))]
     .map(img => img.src)
     .filter(src => src ? true : false )
     .sort()
-elX.style.cssText = 'position:absolute;top:0;left:0;right:0;background:#abc;z-index:10000;border:4px solid black;border-radius:10px'
+elX.style.cssText = 'position:absolute;text-align:center;top:10px;left:10px;right:10px;background:#abc;z-index:10000;border:4px solid black;border-radius:10px'
 elX.innerHTML = '<ul>'
 imgs.forEach(img => elX.innerHTML +=
-    `<li><img style="border:2px solid red" src="${img}"><a href="${img}">${img}</a></li>`
+    `<li style="list-style-type:none"><img style="border:2px solid red" src="${img}"><a href="${img}">${img}</a></li>`
 )
 elX.innerHTML += '</ul>'
 buttonX.addEventListener('click', evt => {
