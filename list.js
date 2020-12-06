f68322056a09474d96d37a4bb4d479e2d063cd8e
Niels Koster <n.koster@portavita.eu>
2020-12-06 20:53:28 +0100

    Image count + Text cleanup

--- list.js
+++ list.js
@@ -6,16 +6,16 @@ imgs = [...new Set(Array.from(document.images))]
     .map(img => img.src)
     .filter(src => src ? true : false )
     .sort()
-elX.style.cssText = 'position:absolute;text-align:center;padding:10px;top:10px;left:10px;right:10px;background:#abc;z-index:10000;border:4px solid black;border-radius:10px'
+elX.style.cssText = 'position:absolute;text-align:center;padding:10px;top:10px;left:10px;right:10px;background:#cbc;z-index:10000;border:4px solid black;border-radius:10px'
 if (imgs.length > 0) {
     elX.innerHTML += `<h3 style="color:black">Image List (${imgs.length} found)</h3>`
     elX.innerHTML += '<ul>'
-    imgs.forEach(img => elX.innerHTML +=
-        `<li style="list-style-type:none;padding:10px"><img style="border:2px solid red;vertical-align:middle" src="${img}"><a style="padding-left:5px;" href="${img}">${img}</a></li>`
+    imgs.forEach((img, index) => elX.innerHTML +=
+        `<li style="list-style-type:none;padding:10px"><img style="border:2px solid red;vertical-align:middle" src="${img}"> ${index + 1} <a style="padding-left:5px;" href="${img}">${img}</a></li>`
     )
     elX.innerHTML += '</ul>'
 } else {
-    elX.innerHTML += '<h3 style="color:black">No images found</h3>'
+    elX.innerHTML += '<h3 style="color:black">No Images Found</h3>'
 }
 buttonX.addEventListener('click', evt => {
     evt.preventDefault()
