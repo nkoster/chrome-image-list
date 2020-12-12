b9f7698cbd1058247ae3afb1fedc0894c5614641
Niels Koster <n.koster@portavita.eu>
2020-12-12 23:00:32 +0100

    map, filter and sort inside Set() (for unique)

--- list.js
+++ list.js
@@ -2,10 +2,11 @@ typeof elX !== 'undefined' && elX.remove()
 elX = document.createElement('div')
 buttonX = document.createElement('button')
 buttonX.textContent = 'Exit Image List'
-imgs = [...new Set(Array.from(document.images))]
-    .map(img => img.src)
-    .filter(src => src ? true : false )
-    .sort()
+imgs = [...new Set(Array.from(document.images)
+        .map(img => img.src)
+        .filter(src => src ? true : false )
+        .sort()
+    )]
 elX.style.cssText = 'overflow:scroll;position:absolute;text-align:center;height:600px;padding:10px;top:20px;left:20px;right:20px;background:#cbc;z-index:10000;border:4px solid black;border-radius:10px'
 if (imgs.length > 0) {
     elX.innerHTML += `<h3 style="color:black">Image List (${imgs.length} found)</h3>`
